26c2f50e6fd72b8a4f7e05aa38c482e27203e0c4
Annece2935 <anne-cecile.colleter@live.fr>
2025-05-08 10:51:41 +0200

    update new form task 6

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-import sys
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    """Handle basic arithmetic operations."""
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    a = sys.argv[1]
-    operator = sys.argv[2]
-    b = sys.argv[3]
-    try:
-        a = int(a)
-        b = int(b)
-    except ValueError:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    operators = {'+': add, '-': sub, '*': mul, '/': div}
-    if operator not in operators:
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    func = operators[operator]
-    if operator == '/' and b == 0:
-        print("Error : Division by zero")
-        sys.exit(1)
-    result = func(a, b)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
     print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
