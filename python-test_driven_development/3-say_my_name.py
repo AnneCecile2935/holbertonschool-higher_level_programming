65b7f614011a614f297ad8648c2fd90d376613b5
Annece2935 <anne-cecile.colleter@live.fr>
2025-05-16 12:51:48 +0200

    update case is empty

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -27,11 +27,13 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    if first_name == "" and last_name == "":
+    first_name = first_name.strip()
+    last_name = last_name.strip()
+    if not first_name and not last_name:
         print("My name is")
-    elif first_name == "":
+    elif not first_name:
         print(f"My name is {last_name}")
-    elif last_name == "":
+    elif not last_name:
         print(f"My name is {first_name}")
     else:
         print(f"My name is {first_name} {last_name}")
