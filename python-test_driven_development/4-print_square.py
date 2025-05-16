1b844cfdb68588655967bc79439d7830b1029ec8
Annece2935 <anne-cecile.colleter@live.fr>
2025-05-16 15:07:56 +0200

    udpate task 4

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module 4-print_square
+Defines a function `print_square(size)` that prints a square using the `#` character.
+
+The function takes an integer `size` as its parameter. If `size` is not an integer
+or if it is less than 0, appropriate exceptions will be raised.
+
+Example:
+    >>> print_square(3)
+    ###
+    ###
+    ###
+"""
+def print_square(size):
+    """
+    Prints a square with the character '#'.
+
+    Args:
+        size (int): The size of the square.
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
