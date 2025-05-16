2733f227950ad9255b9e146af312e4dd8a26b460
Annece2935 <anne-cecile.colleter@live.fr>
2025-05-16 15:32:24 +0200

    update task 3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -23,9 +23,9 @@ def say_my_name(first_name, last_name=""):
         >>> say_my_name("Alice")
         My name is Alice
     """
-    if type(first_name) is not str:
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if type(last_name) is not str:
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
     if last_name:
