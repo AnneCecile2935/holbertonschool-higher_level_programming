e05f71a2713bfae894b76af22dd5bdc9cfc95025
Annece2935 <anne-cecile.colleter@live.fr>
2025-05-16 09:33:01 +0200

    update task 0 test driven for function add

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,41 +1,28 @@
 #!/usr/bin/python3
+"""This module provides basic arithmetic functions.
+It includes add_integer to add two numbers as integers.
+Inputs are validated and exceptions are raised if invalid.
+This helps ensure reliable and safe operations.
+Documentation meets the project requirements."""
+
+
 def add_integer(a, b=98):
     """
-    Adds two integers.
-
+    Adds two integers or floats and returns their sum as an integer.
     Args:
-        a: The first number, must be an integer or float.
-        b: The second number, must be an integer or float (default is 98).
-
-    Returns:
-        The addition of a and b as an integer.
-
+        a (int or float): The first number.
+        b (int or float): The second number, default is 98.
     Raises:
-        TypeError: If a or b is not an integer or float.
-
-    Examples:
-        >>> add_integer(2, 3)
-        5
-        >>> add_integer(100)
-        198
-        >>> add_integer(2.5, 3)
-        5
-        >>> add_integer(2.5, "3")
-        Traceback (most recent call last):
-            ...
-        TypeError: b must be an integer
+        TypeError: If a or b is not an integer or a float.
+    Returns:
+        int: The sum of a and b as an integer.
     """
-    if not isinstance(a, (int, float)):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-
-    a = int(a)
-    b = int(b)
-
-    return a + b
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return (a + b)
