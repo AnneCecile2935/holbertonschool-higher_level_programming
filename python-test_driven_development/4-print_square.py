2b57b9d9c8e40ab962f7abdb026cba2b206a48e8
Annece2935 <anne-cecile.colleter@live.fr>
2025-05-16 15:23:34 +0200

    update task 4 another way

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -27,16 +27,12 @@ def print_square(size):
         TypeError: If size is not an integer.
         ValueError: If size is less than 0.
     """
-    if isinstance(size, float) and size < 0:
-        raise TypeError("size must be an integer")
     if not isinstance(size, int):
+        if isinstance(size, float) and size < 0:
+            raise TypeError("size must be an integer")
         raise TypeError("size must be an integer")
+
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    for _ in range(size):
-        print("#" * size)
-
-    if __name__ == "__main__":
-        import doctest
-        doctest.testfile("4-print_square.txt")
+    print(("#" * size + "\n") * size, end="")
