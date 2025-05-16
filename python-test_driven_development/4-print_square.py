5cba271c44c5bf14f44040ad907b7067f3d96e7c
Annece2935 <anne-cecile.colleter@live.fr>
2025-05-16 15:25:02 +0200

    update task 4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -28,11 +28,10 @@ def print_square(size):
         ValueError: If size is less than 0.
     """
     if not isinstance(size, int):
-        if isinstance(size, float) and size < 0:
-            raise TypeError("size must be an integer")
         raise TypeError("size must be an integer")
 
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    print(("#" * size + "\n") * size, end="")
+    for _ in range(size):
+        print("#" * size)
