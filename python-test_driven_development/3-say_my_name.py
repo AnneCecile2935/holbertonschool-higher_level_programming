615c201f1472275c6c4ba4a5deb1a56ec161e368
Annece2935 <anne-cecile.colleter@live.fr>
2025-05-16 15:29:51 +0200

    update task 3 with "type" and not isinstance

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -23,12 +23,12 @@ def say_my_name(first_name, last_name=""):
         >>> say_my_name("Alice")
         My name is Alice
     """
-    if not isinstance(first_name, str):
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
 
     if last_name:
-        print("My name is {:s} {:s}".format(first_name, last_name))
+        print(f"My name is {first_name} {last_name}")
     else:
-        print("My name is {:s}".format(first_name))
+        print(f"My name is {first_name}")
