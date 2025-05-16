cb1b2d9ca93db7e925ee8eaa6f20c6ccedd77720
Annece2935 <anne-cecile.colleter@live.fr>
2025-05-16 13:09:35 +0200

    update a nother time task 3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -29,6 +29,6 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("last_name must be a string")
 
     if last_name:
-        print(f"My name is {first_name} {last_name}")
+        print("My name is {:s} {:s}".format(first_name, last_name))
     else:
-        print(f"My name is {first_name}")
+        print("My name is {:s}".format(first_name))
