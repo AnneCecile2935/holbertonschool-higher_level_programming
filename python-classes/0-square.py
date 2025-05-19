4bb2e65d6894ee3371e970295303ccb8d6ebe4ec
Annece2935 <anne-cecile.colleter@live.fr>
2025-05-19 09:57:03 +0200

    add class empty to Square and documentation for module, class and function init

---
+++ python-classes/0-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module defines the Square class.
+The Square class is currently a basic structure with no attributes or methods.
+"""
+
+
+class Square :
+    """
+    A class to represent a square.
+
+    Methods:
+        __init__(): Initializes a new instance of the Square class.
+    """
+    def __init__(self):
+        """
+        Initializes a new Square instance.
+        Currently, this constructor does not take any parameters or define any attributes.
+        """
+        pass
