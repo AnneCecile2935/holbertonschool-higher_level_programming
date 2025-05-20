c5eabde1c9e5e2db2fd74d1eaba8e15d17e5607a
Annece2935 <anne-cecile.colleter@live.fr>
2025-05-20 09:50:29 +0200

    empty Rectangle class and create readme of folder, all with comments

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module defines the Rectangle class.
+The Rectangle class is currently a basic structure with no attributes
+or methods.
+"""
+
+
+class Rectangle:
+    """
+    A class to represent a rectangle.
+
+    Methods:
+        __init__(): Initializes a new instance of the Rectangle class.
+    """
+
+
+def __init__(self):
+    """
+        Initializes a new Square instance.
+        Currently, this constructor does not take any parameters
+        or define any attributes.
+    """
+    pass
