8ce1149c238be397d6a9a73723afb06f891de4a7
Annece2935 <anne-cecile.colleter@live.fr>
2025-05-26 10:33:34 +0200

    update with moidule comment and return list

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+"""
+Module lookup
+
+Ce module contient une fonction `lookup` qui retourne la liste des attributs
+et méthodes disponibles pour un objet donné.
+
+Cette fonction permet d'explorer dynamiquement un objet en Python,
+utile notamment pour le débogage, la documentation ou l’inspection
+du comportement des objets.
+"""
+
+
 def lookup(obj):
     """
     Retourne la liste des attributs et méthodes disponibles
@@ -15,4 +27,4 @@ def lookup(obj):
         >>> lookup("hello")
         ['__add__', '__class__', ..., 'upper', 'zfill']
     """
-    return dir(obj)
+    return dir(list)
