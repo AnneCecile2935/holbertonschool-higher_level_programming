2042ea28d35ea184470cb0ae5137d536f9981e48
Annece2935 <anne-cecile.colleter@live.fr>
2025-05-26 13:18:23 +0200

    add empty class task 5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module base_geometry
+
+Ce module contient la fonction base_geometry est une classe vide
+"""
+
+
+class BaseGeometry:
+    """
+    Le classe BaseGeometry est une classe vide utilisée comme classe de base
+    pour la suite de l'exercice
+
+    Cette classe ne contient actuellement aucun attribut ni méthode,
+    mais peut être étendue dans le futur
+    """
+
+    {}
