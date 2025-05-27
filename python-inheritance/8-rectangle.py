03bc1f7c26c03c3b81694efc4ba9e10a8e68addd
Annece2935 <anne-cecile.colleter@live.fr>
2025-05-27 09:54:52 +0200

    update task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,53 +1,6 @@
 #!/usr/bin/python3
-"""
-Module base_geometry
-
-Ce module définit une hiérarchie de classes pour des formes géométriques.
-
-Il contient :
-- une classe de base `BaseGeometry`, qui fournit une interface générique
-  pour des formes géométriques, incluant une méthode à implémenter (`area`)
-  et une méthode utilitaire de validation (`integer_validator`),
-- une sous-classe `Rectangle`, représentant un rectangle avec largeur
-  et hauteur validées.
-"""
-
-
-class BaseGeometry:
-    """
-    Classe de base pour des formes géométriques.
-
-    Fournit une interface commune et des méthodes de validation,
-    destinée à être héritée par d'autres classes géométriques.
-    """
-
-    def area(self):
-        """
-        Méthode non implémentée, à surcharger dans les sous-classes.
-
-        Raises:
-            Exception: Toujours levée pour indiquer que la méthode
-            n'est pas encore implémentée.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Valide que `value` est un entier strictement positif.
-
-        Args:
-            name (str): Le nom de l'attribut (utilisé dans les messages
-            d'erreur).
-            value (int): La valeur à valider.
-
-        Raises:
-            TypeError: Si value n'est pas un entier.
-            ValueError: Si value est inférieur ou égal à 0.
-        """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+'''Module for Rectangle class.'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
