fd0b2d52d349e5bee5ec3263be2a81e3a763473e
Annece2935 <anne-cecile.colleter@live.fr>
2025-05-27 10:07:58 +0200

    update task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -14,24 +14,7 @@ class MyList(list):
     des entiers et ajoute une méthode `print_sorted` pour afficher
     la liste triée.
     """
-    def __init__(self, entier=None):
-        """
-        Initialise une nouvelle instance de MyList.
-
-        Args:
-            entier (list, optional): Liste d'entiers à initialiser.
-                                     Par défaut, une liste vide est utilisée.
-
-        Raises:
-            TypeError: Si un ou plusieurs éléments de la liste ne sont
-            pas des entiers.
-        """
-        if entier is None:
-            entier = []
-        if not all(isinstance(value, int) for value in entier):
-            raise TypeError("Elements must be integers")
-        super().__init__(entier)
-
+ 
     def print_sorted(self):
         """
         Affiche les éléments de la liste dans l'ordre croissant (trié),
