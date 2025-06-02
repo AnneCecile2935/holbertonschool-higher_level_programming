ae87f0fbe15c0b15a08ab8eb3da05b92001a0355
Annece2935 <anne-cecile.colleter@live.fr>
2025-06-02 07:50:35 +0200

    add task 0 read a file with "with"

--- python-inpout_output/0-read_file.py
+++ python-inpout_output/0-read_file.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+This module provides a function to read and display the contents
+of a text file.
+"""
+def read_file(filename=""):
+    """
+    Reads a text file (UTF-8) and prints its contents to stdout.
+
+    Args:
+        filename (str): The path to the file to be read.
+        Defaults to an empty string.
+
+    Raises:
+        FileNotFoundError: If the file does not exist.
+        PermissionError: If the file cannot be accessed due
+        to permission issues.
+    """
+    with open("my_file_0.txt", "rt") as f:
+        print(f.read())
