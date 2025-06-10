90e2e24a10e3bbdc6af552b901cea2da5568fe63
Annece2935 <anne-cecile.colleter@live.fr>
2025-06-10 12:12:24 +0200

    add info

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -20,6 +20,14 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b"OK")
 
+        elif self.path == '/info':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            info = {"version": "1.0", "description": "A simple API built with http.server"}
+            json_info = json.dumps(info)
+            self.wfile.write(json_info.encode("utf-8"))
+
         else:
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
