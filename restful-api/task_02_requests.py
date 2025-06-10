1fae949a54f1320fecc173dffc9f4f4dd3dbc8a9
Annece2935 <anne-cecile.colleter@live.fr>
2025-06-10 09:55:39 +0200

    update task 2

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -52,7 +52,7 @@ def fetch_and_save_posts():
             for post in posts
         ]
 
-    with open("posts.csv", 'w', newline='', encoding='utf-8') as csv_file:
+    with open("posts.csv", 'w', newline='') as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=['id', 'title', 'body'])
         writer.writeheader()
         writer.writerows(data)
