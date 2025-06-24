8ee0eb822108edb446e4d2389517fcc3c1a0bbbf
Annece2935 <anne-cecile.colleter@live.fr>
2025-06-24 18:09:08 +0200

    update task 2 with create db

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,5 @@
+-- create DATA BASE
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Créer l'utilisateur s'il n'existe pas déjà
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
