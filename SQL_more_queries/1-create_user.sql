57c171e9f657df61c0bcbc259422b610b3457ea4
Annece2935 <anne-cecile.colleter@live.fr>
2025-06-24 17:44:53 +0200

    update task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,9 +1,9 @@
 -- Créer l'utilisateur s'il n'existe pas déjà
 CREATE USER
-	IF NOT EXISTS 'user_0d_1'@'localhost'
-	IDENTIFIED BY 'user_0d_1_pwd';
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
-	ON *.*
-	TO 'user_0d_1'@'localhost'
-	IDENTIFIED BY 'user_0d_1_pwd';
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
