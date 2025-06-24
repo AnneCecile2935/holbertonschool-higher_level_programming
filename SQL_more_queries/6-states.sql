50ae8a3f540ce29e1c9d181b3de8ef6685ac9901
Annece2935 <anne-cecile.colleter@live.fr>
2025-06-24 18:54:58 +0200

    update task 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,7 @@
---create db ans table with id auto generated
+-- create db ans table with id auto generated
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS states(
 	id INT AUTO_INCREMENT PRIMARY KEY,
