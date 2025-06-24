68f2a084fc7107cf1d2c8220c4508bea18aaef80
Annece2935 <anne-cecile.colleter@live.fr>
2025-06-24 18:20:35 +0200

    update task 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,11 +1,15 @@
 -- create DATA BASE
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE DATABASE
+IF NOT EXISTS hbtn_0d_2;
 -- Créer l'utilisateur s'il n'existe pas déjà
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+CREATE USER
+IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Donner uniquement le droit SELECT à cet utilisateur
-GRANT SELECT ON hbtn_0d_2 *TO 'user_0d_2'@'localhost';
+GRANT SELECT
+ON hbtn_0d_2.*
+TO 'user_0d_2'@'localhost';
 
 -- Appliquer les changements
 FLUSH PRIVILEGES;
