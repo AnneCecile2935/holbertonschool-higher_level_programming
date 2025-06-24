c863b3003fce5531becafe59fd453f064b239529
Annece2935 <anne-cecile.colleter@live.fr>
2025-06-24 18:12:30 +0200

    update acceed only at db

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -5,7 +5,7 @@ CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Donner uniquement le droit SELECT à cet utilisateur
-GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2 *TO 'user_0d_2'@'localhost';
 
 -- Appliquer les changements
 FLUSH PRIVILEGES;
