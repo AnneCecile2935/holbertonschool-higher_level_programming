b0600a985b505b317106850f91fa1f601ac09205
Annece2935 <anne-cecile.colleter@live.fr>
2025-06-24 17:30:47 +0200

    add function to create user in good condition : privileges and immiediatly

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,6 @@
--- create user
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
+-- create user if not exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- with all privilege on data bases
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'local_host';
+-- apply the changes
+FLUSH PRIVILEGES;
