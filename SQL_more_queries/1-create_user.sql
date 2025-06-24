ea1aab6bd5445f73575631d6fb4a053ddee3cbc7
Annece2935 <anne-cecile.colleter@live.fr>
2025-06-24 17:43:00 +0200

    update task 1 indentation

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,9 @@
 -- Créer l'utilisateur s'il n'existe pas déjà
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Lui accorder tous les privilèges sur le serveur
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
+CREATE USER
+	IF NOT EXISTS 'user_0d_1'@'localhost'
+	IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+	ON *.*
+	TO 'user_0d_1'@'localhost'
+	IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
