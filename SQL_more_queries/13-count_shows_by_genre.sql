771d88a9e6713e92a6bae551c7637c82a5117a49
Annece2935 <anne-cecile.colleter@live.fr>
2025-06-25 07:44:21 +0200

    update task 13 (with not display if count < 0)

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,9 +4,11 @@ SELECT
   COUNT(tv_show_genres.show_id) AS number_of_shows -- Le nombre de shows liés à ce genre
 FROM
   tv_genres                              -- Table des genres
-LEFT JOIN
+JOIN
   tv_show_genres ON tv_genres.id = tv_show_genres.genre_id  -- Jointure pour associer les shows aux genres
 GROUP BY
-  tv_genres.id                          -- Regroupe les résultats par genre
+  tv_genres.id
+HAVING
+	number_of_shows > 0                          -- Regroupe les résultats par genre
 ORDER BY
   number_of_shows DESC;                          -- Trie les résultats par nombre de shows décroissant
