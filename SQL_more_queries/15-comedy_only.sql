1a8c2c2bfd856d36f48f50c58921fc181a594f66
Annece2935 <anne-cecile.colleter@live.fr>
2025-06-25 08:03:13 +0200

    update task 15

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- list of comedy
-SELECT tv_shows_title As title
+SELECT tv_shows.title As title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
