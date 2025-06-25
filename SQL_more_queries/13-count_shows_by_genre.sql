e29cdbd8c0941bdff25db664cccfe303225b9b4d
Annece2935 <anne-cecile.colleter@live.fr>
2025-06-25 07:39:33 +0200

    add task 13, groupe par genre et compter nb de film par genre, afficher dans ordre croissant

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,12 @@
+-- Sélectionne le nom du genre et le nombre de shows associés
+SELECT
+  tv_genres.name AS genre,               -- Le nom du genre
+  COUNT(tv_show_genres.show_id) AS count -- Le nombre de shows liés à ce genre
+FROM
+  tv_genres                              -- Table des genres
+LEFT JOIN
+  tv_show_genres ON tv_genres.id = tv_show_genres.genre_id  -- Jointure pour associer les shows aux genres
+GROUP BY
+  tv_genres.id                          -- Regroupe les résultats par genre
+ORDER BY
+  count DESC;                          -- Trie les résultats par nombre de shows décroissant
