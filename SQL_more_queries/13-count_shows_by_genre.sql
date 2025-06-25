263245d5be2a58230b9dc5bdbc9759988d7af4a8
Annece2935 <anne-cecile.colleter@live.fr>
2025-06-25 07:40:53 +0200

    update task 13 with good name column

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- Sélectionne le nom du genre et le nombre de shows associés
 SELECT
   tv_genres.name AS genre,               -- Le nom du genre
-  COUNT(tv_show_genres.show_id) AS count -- Le nombre de shows liés à ce genre
+  COUNT(tv_show_genres.show_id) AS number_of_shows -- Le nombre de shows liés à ce genre
 FROM
   tv_genres                              -- Table des genres
 LEFT JOIN
@@ -9,4 +9,4 @@ LEFT JOIN
 GROUP BY
   tv_genres.id                          -- Regroupe les résultats par genre
 ORDER BY
-  count DESC;                          -- Trie les résultats par nombre de shows décroissant
+  number_of_shows DESC;                          -- Trie les résultats par nombre de shows décroissant
