8be4e4246a89ccbb7cb112590630151d9d85b78a
Annece2935 <anne-cecile.colleter@live.fr>
2025-06-26 11:35:35 +0200

    update task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -12,20 +12,17 @@ Example:
 import sys
 import MySQLdb
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name_searched = sys.argv[4]
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
-        passwd=password,
-        db=database_name
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
         )
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC;".format(
-        state_name_searched)
+    query = "SELECT * FROM states WHERE BINARY name = '{}' ".format(
+        sys.argv[4])
     cursor.execute(query)
     rows = cursor.fetchall()
 
