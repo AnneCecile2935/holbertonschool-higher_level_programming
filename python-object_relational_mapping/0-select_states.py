64678bdb8ba780f8afbcb0f051546a62a3a70d7e
Annece2935 <anne-cecile.colleter@live.fr>
2025-06-26 10:53:03 +0200

    update task 0 pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """
-Script qui se connecte à une base de données MySQL et affiche toutes les entrées
+Script qui se connecte à une base de données MySQL et affiche toutes
+les entrées
 de la table `states` triées par ordre croissant selon leur `id`.
 
-Le script prend en arguments la connexion MySQL (nom d'utilisateur, mot de passe,
+Le script prend en arguments la connexion MySQL (nom d'utilisateur,
+mot de passe,
 nom de la base de données) et affiche chaque ligne de la table `states`.
 
 Usage:
@@ -17,7 +19,7 @@ import MySQLdb
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    db_name = sys.argv[3]
+    database_name = sys.argv[3]
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
