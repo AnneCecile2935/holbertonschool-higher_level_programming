3402a7d37bfab04d2201887bbe32bf56fd9d07f3
Annece2935 <anne-cecile.colleter@live.fr>
2025-06-26 10:51:04 +0200

    update task 0 with documentation

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+"""
+Script qui se connecte à une base de données MySQL et affiche toutes les entrées
+de la table `states` triées par ordre croissant selon leur `id`.
+
+Le script prend en arguments la connexion MySQL (nom d'utilisateur, mot de passe,
+nom de la base de données) et affiche chaque ligne de la table `states`.
+
+Usage:
+    ./0-select_states.py <username> <password> <database_name>
+
+Exemple:
+    ./0-select_states.py root mypassword hbtn_0e_0_usa
+"""
 import sys
 import MySQLdb
 if __name__ == "__main__":
