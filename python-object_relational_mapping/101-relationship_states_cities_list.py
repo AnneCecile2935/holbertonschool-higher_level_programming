f984853c2a76186a3a72b0f9b8d93953a4b5de27
Annece2935 <anne-cecile.colleter@live.fr>
2025-06-28 09:46:46 +0200

    add task 16 display states and cities onlly one query with join and relationship between state and city

---
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+Lists all State objects and their linked City objects from a MySQL database.
+
+Usage:
+    ./101-relationship_states_cities_list.py <mysql_username> <mysql_password>
+      <database_name>
+
+This script:
+- Connects to a MySQL server on localhost:3306 using SQLAlchemy.
+- Uses a single query to fetch all State objects and preloads their cities via
+ relationship().
+- Displays the results sorted by states.id and cities.id (ascending).
+- Prints the data in the following format:
+
+    <state id>: <state name>
+        <city id>: <city name>
+
+Note:
+    The relationship between State and City must be defined using SQLAlchemy's
+    ORM
+    with joined loading to avoid N+1 queries. Your State class must have a
+    `cities` relationship to City.
+"""
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from relationship_city import City
+from relationship_state import State, Base
+from sqlalchemy.orm import relationship, joinedload
+
+if __name__ == "__main__":
+    # connect to MySQl server using SQLAlchemy
+    engine = create_engine(
+        "mysql+mysqldb://"
+        f"{sys.argv[1]}:{sys.argv[2]}"
+        "@localhost:3306/"
+        f"{sys.argv[3]}",
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    # Query all States and preload their related cities in one query
+    states = (
+        session.query(State)
+        .options(joinedload(State.cities))
+        .order_by(State.id).
+        all()
+        )
+    # options(joined(state.cities = charge tous les objets State
+    # en la relation cities avec un Join SQl,
+    # requête qui combine State et City avec un JOIN
+
+    # Iterate over states and their cities,
+    # displaying them in the required format
+    for state in states:
+        print(f"{state.id}: {state.name}")
+        # Sort cities by city.id ascending
+        for city in sorted(state.cities, key=lambda c: c.id):
+            print(f"    {city.id}: {city.name}")
+
+    session.close()
