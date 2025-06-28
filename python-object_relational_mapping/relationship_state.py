8a3caf98727f16d49870ed41b60808eb2c1a0ded
Annece2935 <anne-cecile.colleter@live.fr>
2025-06-28 08:55:52 +0200

    add task 15 relationship one to many and create ne state and city

---
+++ python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Definition du modèle State pour SQLAlchemy ORM.
+
+Ce module définit la classe `State`, qui est mappée à la table `states`
+dans une base de données MySQL.
+Elle permet de manipuler des enregistrements de la table `states`
+en utilisant SQLAlchemy.
+
+Classes:
+    State: classe représentant un état avec un identifiant et un nom.
+
+Attributs:
+    Base: instance de la classe declarative_base utilisée pour la
+    déclaration du modèle ORM.
+"""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Classe `State` liée à la table `states` dans la base de données.
+
+    Attributs:
+        id (int): Clé primaire de la table, auto-incrémentée et non nulle.
+        name (str): Nom de l'état, chaîne de caractères non nulle de longueur
+        maximale 128.
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+# establish one-to-many relationship
+    cities = relationship(
+        "City",
+        backref='state',
+        cascade='all, delete'
+    )
