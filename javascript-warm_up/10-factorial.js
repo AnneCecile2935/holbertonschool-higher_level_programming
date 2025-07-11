6c5181788e49d165d989551dae51ae1a14586f44
Annece2935 <anne-cecile.colleter@live.fr>
2025-07-11 15:07:59 +0200

    add task 10 factorial

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const num = Number(process.argv[2]);
+function factorial (n) {
+  if (isNaN(n) || n < 0) {
+    return 1;
+  } else if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+console.log(factorial(num));
