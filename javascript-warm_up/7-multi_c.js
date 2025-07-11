2eed6ba536eb200e5e4f2c440eef40a5e8fc51d5
Annece2935 <anne-cecile.colleter@live.fr>
2025-07-11 14:43:57 +0200

    add task 7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const args = Number(process.argv[2]);
+const text = 'C is fun';
+if (Number.isInteger(args) && args > 0) {
+  for (let i = 0; i < args; i++) {
+    console.log(text);
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
