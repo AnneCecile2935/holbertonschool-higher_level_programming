1db45c70a71e9e5fd41ee7635d1b3142c7b6955a
Annece2935 <anne-cecile.colleter@live.fr>
2025-07-11 14:50:25 +0200

    update task 7 if number is negative nothing to print

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const args = Number(process.argv[2]);
 const text = 'C is fun';
-if (Number.isInteger(args) && args > 0) {
+if (isNaN(args)) {
+  console.log('Missing number of occurrences');
+} else if (args >= 0) {
   for (let i = 0; i < args; i++) {
     console.log(text);
   }
-} else {
-  console.log('Missing number of occurrences');
 }
