4545d6828c52e664ff17f9f74e8abdc7cb37e98b
Annece2935 <anne-cecile.colleter@live.fr>
2025-07-11 14:44:21 +0200

    Add

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const num1 = Number(process.argv[2]);
+const num2 = Number(process.argv[3]);
+
+function add (num1, num2) {
+  return num1 + num2;
+}
+const x = add(num1, num2);
+console.log(x);
