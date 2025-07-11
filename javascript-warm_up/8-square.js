819e515444d44295586b3598add3ec2f84e0bc2e
Annece2935 <anne-cecile.colleter@live.fr>
2025-07-11 14:44:09 +0200

    Square

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const size = Number(process.argv[2]);
+const text = 'X';
+if (Number.isInteger(size) && size > 0) {
+  for (let i = 0; i < size; i++) {
+    console.log(text.repeat(size));
+  }
+} else {
+  console.log('Missing size');
+}
